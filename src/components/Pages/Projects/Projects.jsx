5277e3f2e86da4355529cd9e3717f997af1ca0ce
Vincentius Kenton <132326189+tersorasora@users.noreply.github.com>
2025-08-12 22:13:58 +0700

    minor update

--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -5,8 +5,7 @@ import transition from '../../Transition/Transition';
 const Projects = () => {
   return (
     <div className="project">
-      <h1>Welcome to My Project Portfolio Page</h1>
-      <p>This is the project page where you can find an overview of my work and skills.</p>
+      
     </div>
   );
 }
