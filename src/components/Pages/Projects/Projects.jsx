711246b75ea5f0b30d7a9b140bffe9010aae206e
Vincentius Kenton <132326189+tersorasora@users.noreply.github.com>
2025-08-13 21:21:55 +0700

    add icon, animation, and projects page

--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Projects.css';
 import transition from '../../Transition/Transition';
 
 const Projects = () => {
+  const projectNames = [
+    "ATMA KITCHEN",
+    "SOULS MERGE",
+    "INSTRUMENT OPERATING SYSTEM",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevProject = () => {
+    setCurrentIndex((prev) => 
+      prev === 0 ? 0 : prev - 1
+    );
+  };
+
+  const nextProject = () => {
+    setCurrentIndex((prev) => 
+      prev === projectNames.length - 1 ? projectNames.length - 1  : prev + 1
+    );
+  };
+
   return (
-    <div className="project">
-      
+    <div className="background-project">
+      <div className="project">
+        <div className="project-name">
+          <button className="arrow-btn" onClick={prevProject}><i className="fas fa-caret-left"></i></button>
+          <h2>{projectNames[currentIndex]}</h2>
+          <button className="arrow-btn" onClick={nextProject}><i className="fas fa-caret-right"></i></button>
+        </div>
+      </div>
     </div>
   );
 }
