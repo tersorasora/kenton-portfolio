54878dfbb394f226f6c003f5a15c64b0a8ae2e90
Vincentius Kenton <132326189+tersorasora@users.noreply.github.com>
2025-08-16 23:00:44 +0700

    add something in projects page

--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -4,19 +4,29 @@ import transition from '../../Transition/Transition';
 
 const projectList = [
   {
-    name: "ATMA KITCHEN",
+    name: "ATMA",
+    nameSecond: "KITCHEN",
     description: "A web-based platform for online food ordering, integrated with a smart system to manage food expiration and reduce waste.",
     buildTech: "Laravel, Bootstrap, MySQL Database, HTML, CSS, JavaSCript, PHP",
+    projectType: "WEBSITE",
+    dificulty: "4",
   },
   {
-    name: "SOULS MERGE",
+    name: "SOULS",
+    nameSecond: "MERGE",
     description: "A 2D pixel art game featuring souls-like open-world exploration and core RPG mechanics.",
     buildTech: "Unity, C#, Aseprite",
+    projectType: "DESKTOP GAME",
+    dificulty: "6",
   },
   {
-    name: "INSTRUMENT OPERATING SYSTEM",
+    name: "INSTRUMENT",
+    nameSecond: "OPERATING",
+    nameThird: "SYSTEM",
     description: "UI control system for the Indonesian Air Force flight simulation, integrated with X-Plane 12 DataRef.",
     buildTech: "Windows Forms, C#, C++, .Net Framework, X-Plane SDK",
+    projectType: "WINDOWS APPLICATION",
+    dificulty: "7",
   }
 ];
 
@@ -40,11 +50,34 @@ const Projects = () => {
       <div className="project">
         <div className="project-name">
           <button className="arrow-btn" onClick={prevProject}><i className="fas fa-caret-left"></i></button>
-          <h2>{projectList[currentIndex].name}</h2>
+          <h2>
+            <span className='name-first'>{projectList[currentIndex].name}</span>
+            <span className='name-second'>{projectList[currentIndex].nameSecond}</span>
+            {projectList[currentIndex].nameThird && (
+              <span className="name-third">{projectList[currentIndex].nameThird}</span>
+            )}
+          </h2>
           <button className="arrow-btn" onClick={nextProject}><i className="fas fa-caret-right"></i></button>
         </div>
         <div className="project-content">
-          
+          <div className="project-images">
+
+          </div>
+          <div className="project-texts">
+            <div className="project-type">
+              <h3>{projectList[currentIndex].projectType}</h3>
+            </div>
+            <div className="project-dificulty">
+              <h3>Dificulties : {projectList[currentIndex].dificulty} &#9733;&#9733;&#9733;&#9734;&#9734;</h3>
+            </div>
+            <div className="project-description">
+              <p>{projectList[currentIndex].description}</p>
+            </div>
+            <div className="project-build-tech">
+              <h3>Build with:</h3>
+              <p>{projectList[currentIndex].buildTech}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
