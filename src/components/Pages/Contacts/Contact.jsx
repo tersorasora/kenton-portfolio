77b9d00f79e1998d2bfdd89f60175f66688cec16
Vincentius Kenton <132326189+tersorasora@users.noreply.github.com>
2025-08-22 13:08:29 +0700

    experiences and contact me

--- src/components/Pages/Contacts/Contact.jsx
+++ src/components/Pages/Contacts/Contact.jsx
@@ -4,9 +4,18 @@ import transition from '../../Transition/Transition';
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <h1>Welcome to My Contact Page</h1>
-      <p>This is the contact page where you can find an overview of my work and skills.</p>
+    <div className="contact-background">
+      <div className="contact-area">
+        <div className="contact-question">
+          <h1>GET IN TOUCH WITH ME??</h1>
+        </div>
+        <div className="contact-content">
+          <h2 className='contact-linkedIn'>Touch me On LinkedIn</h2>
+          <h2 className='contact-email'>Touch me On Email</h2>
+          <h2 className='contact-wa'>Touch me On WA</h2>
+          <h2 className='contact-ig'>Touch me On Instagram</h2>
+        </div>
+      </div>
     </div>
   );
 }
