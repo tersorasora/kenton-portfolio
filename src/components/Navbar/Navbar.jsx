89dd7eefe197186056d8e34b5238f875c5668c04
Vincentius Kenton <132326189+tersorasora@users.noreply.github.com>
2025-08-22 22:09:50 +0700

    navbar logo design update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,20 @@ const Navbar = () => {
 
   return (
     <nav className='navbar'>
-      <div className='logo'>PORTFOLIO</div>
+      <div className='logo'>
+        {/* PORTFOLIO */}
+        <h1>
+          <span className='P'>P</span>
+          <span className='O'>O</span>
+          <span className='R'>R</span>
+          <span className='T'>T</span>
+          <span className='F'>F</span>
+          <span className='O'>O</span>
+          <span className='L'>L</span>
+          <span className='I'>I</span>
+          <span className='O2'>O</span>
+        </h1>
+      </div>
       <ul className='nav-links'>
         <li className={currentIndex === 0 ? 'active' : ''}><Link to="/kenton-portfolio/" onClick={() => setNextIndex(0)}>HOME</Link></li>
         <li className={currentIndex === 1 ? 'active' : ''} ><Link to="/kenton-portfolio/about" onClick={() => setNextIndex(1)}>ABOUT ME</Link></li>
