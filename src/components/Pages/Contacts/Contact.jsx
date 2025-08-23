4778a8476ba0c1523f54e7bcd10cd2c559e994bd
Vincentius Kenton <132326189+tersorasora@users.noreply.github.com>
2025-08-24 00:16:50 +0700

    message sent notification!!

--- src/components/Pages/Contacts/Contact.jsx
+++ src/components/Pages/Contacts/Contact.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Modal from 'react-modal';
 import './Contact.css';
 import transition from '../../Transition/Transition';
 import emailjs from '@emailjs/browser';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Contact = () => {
-const [showEmailForm, setShowEmailForm] = useState(false);
-const form = useRef();
+  const [showEmailForm, setShowEmailForm] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -22,8 +24,22 @@ const form = useRef();
         (error) => {
           console.log('FAILED...', error.text);
         },
+        setShowEmailForm(false),
+        setTimeout(() => {
+          setShowNotification(true);
+        }, 200)
       );
   };
+
+  useEffect(() => {
+    let timer;
+    if (showNotification === true) {
+      timer = setTimeout(() => {
+        setShowNotification(false);
+      }, 2000); // 2 seconds
+    }
+    return () => clearTimeout(timer);
+  }, [showNotification]);
 
   return (
     <div className="contact-background">
@@ -32,52 +48,72 @@ const form = useRef();
           <h1>REACH ME ANYTIME!!</h1>
         </div>
         <div className="contact-content">
-          <a onClick={() => setShowEmailForm(!showEmailForm)}>
-            <h2 className='contact-ig'>Send Me an&nbsp;<span style={{color: '#fc1c0cff'}}>E-Mail<i className="fa-solid fa-envelope"></i></span></h2>
+          <a onClick={() => setShowEmailForm(true)}>
+            <h2 className='contact-ig'>Send Me an&nbsp;<span style={{ color: '#fc1c0cff' }}>E-Mail<i className="fa-solid fa-envelope"></i></span></h2>
           </a>
           <a href="https://www.linkedin.com/in/vincentius-kenton" target="_blank" rel="noopener noreferrer">
-            <h2>Check My Proflie on&nbsp;<span style={{color: '#456df3ff'}}>LinkedIn<i className="fa-brands fa-linkedin"></i></span></h2>
+            <h2>Check My Proflie on&nbsp;<span style={{ color: '#456df3ff' }}>LinkedIn<i className="fa-brands fa-linkedin"></i></span></h2>
           </a>
           <a href="https://github.com/tersorasora" target='_blank' rel="noopener noreferrer">
             <h2>Check Out My Projects on&nbsp;<span>Github<i className="fa-brands fa-square-github"></i></span></h2>
           </a>
           <a href="https://wa.me/+6281391338726" target='_blank' rel='noopener noreferrer'>
-            <h2>Message Me on&nbsp;<span style={{color: '#1fd328ff'}}>WhatsApp<i className="fa-brands fa-whatsapp"></i></span></h2>
+            <h2>Message Me on&nbsp;<span style={{ color: '#1fd328ff' }}>WhatsApp<i className="fa-brands fa-whatsapp"></i></span></h2>
           </a>
           <a href="https://www.instagram.com/sora_0068/" target='_blank' rel='noopener noreferrer'>
-            <h2>Follow Me on&nbsp;<span style={{color: '#f5197cff'}}>Instagram<i className="fa-brands fa-instagram"></i></span></h2>
+            <h2>Follow Me on&nbsp;<span style={{ color: '#f5197cff' }}>Instagram<i className="fa-brands fa-instagram"></i></span></h2>
           </a>
         </div>
       </div>
 
       {/* Email Form modal */}
-      {showEmailForm && (
-        <div className="email-form-modal">
-          <div className="email-form-content">
-            <div className="form-header">
-              <h2>SEND ME AN E-MAIL</h2>
-              <span className="close-button" onClick={() => setShowEmailForm(false)}><i class="fa-solid fa-xmark"></i></span>
-            </div>
-            <form ref={form} onSubmit={sendEmail} className="email-form">
-              <div className="form-body">
-                <div className="form-section">
-                  <h3 className='form-name-text'>NAME</h3>
-                  <input type="text" name="user_name" placeholder="Hanegawa Sora" required />
-                </div>
-                <div className="form-section">
-                  <h3 className='form-email-text'>E-MAIL</h3>
-                  <input type="email" name="user_email" placeholder="SoraH@gmail.com" required />
-                </div>
-                <div className="form-section-message">
-                  <h3 className='form-message-text'>MESSAGE</h3>
-                  <textarea name="message" placeholder="Your Message" required></textarea>
-                </div>
+      <AnimatePresence mode="wait">
+        {showEmailForm && (
+          <motion.div
+            initial={{ scale: 0.5, opacity: 1 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.5, opacity: 0 }}
+            transition={{ duration: 0.1, ease: "easeInOut" }}
+            className="email-form-modal"
+          >
+            <div className="email-form-content">
+              <div className="form-header">
+                <h2>SEND ME AN E-MAIL</h2>
+                <span className="close-button" onClick={() => setShowEmailForm(false)}><i className="fa-solid fa-xmark"></i></span>
               </div>
-              <button type="submit" value="Send" className='send-button'>Send</button>
-            </form>
-          </div>
-        </div>
-      )}
+              <form ref={form} onSubmit={sendEmail} className="email-form">
+                <div className="form-body">
+                  <div className="form-section">
+                    <h3 className='form-name-text'>NAME</h3>
+                    <input type="text" name="user_name" placeholder="Enter your name" required />
+                  </div>
+                  <div className="form-section">
+                    <h3 className='form-email-text'>E-MAIL</h3>
+                    <input type="email" name="user_email" placeholder="Enter your email" required />
+                  </div>
+                  <div className="form-section-message">
+                    <h3 className='form-message-text'>MESSAGE</h3>
+                    <textarea name="message" placeholder="Your Message" required></textarea>
+                  </div>
+                </div>
+                <button type="submit" value="Send" className='send-button'>Send</button>
+              </form>
+            </div>
+          </motion.div>
+        )}
+        {showNotification && (
+          <motion.div
+            initial={{ x: -800, opacity: 0.5 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 400, opacity: 0 }}
+            transition={{ duration: 0.1, ease: "easeInOut" }}
+            className="email-notification"
+          >
+            <h3 className='notify-message'>MESSAGE</h3>
+            <h3 className='notify-sent'>SENT!</h3>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
